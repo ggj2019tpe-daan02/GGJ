a130efac5a8cf5d908067c365cca79576d9571fc
Hans Chiu <chiuhans111@gmail.com>
2019-01-27 13:41:36 +0800

    fadeScreen

---
+++ Assets/Scripts/UI/BlackMOOD_OUT.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackMOOD_OUT : MonoBehaviour {
+    public float FadeSpeed = 1;
+
+    float startTime = 0;
+    // Use this for initialization
+    void Start () {
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var color = GetComponent<UnityEngine.UI.Image>().color;
+        color.a = (1 - Time.time / FadeSpeed);
+        if (color.a < 0) color.a = 0;
+
+        GetComponent<UnityEngine.UI.Image>().color = color;
+
+    }
+}
