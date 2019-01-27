4801d32eb91fc57d594c6c3fcab9da3181a8a9e1
Hans Chiu <chiuhans111@gmail.com>
2019-01-27 13:26:36 +0800

    title scene

---
+++ Assets/Scripts/UI/TitlePageScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitlePageScript : MonoBehaviour {
+    public _SceneManager manager;
+    bool started = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    // Update is called once per frame
+    void Update () {
+
+        if (!started && Input.GetButton("Jump")) {
+            started = true;
+            manager.LoadScene("Main");
+        }
+	}
+}
---
+++ Assets/Scripts/UI/TitleTextBlink.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleTextBlink : MonoBehaviour {
+
+
+
+    public float BlinkSpeed = 200;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var color = GetComponent<UnityEngine.UI.Text>().color;
+        if (Time.time / BlinkSpeed % 2 <= 1)
+            color.a = 0;
+        else color.a = 1;
+
+        GetComponent<UnityEngine.UI.Text>().color = color;
+    }
+}
