cb97ba15557b649e7b812588554a99b08b752bd1
Hans Chiu <chiuhans111@gmail.com>
2019-01-27 12:26:54 +0800

    TitlePage updated

---
+++ Assets/Scripts/UI/TitleGhost.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleGhost : MonoBehaviour {
+
+    Vector3 Origin;
+
+	// Use this for initialization
+	void Start () {
+        Origin = transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Origin + new Vector3(
+            Mathf.Sin(Time.time/5) * 40f, 
+            Mathf.Sin(Time.time) * 30f, 0);
+
+    }
+}
---
+++ Assets/Scripts/UI/TitleOtaku.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleOtaku : MonoBehaviour {
+
+    Vector3 originalSize;
+
+
+	// Use this for initialization
+	void Start () {
+        originalSize = transform.localScale;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float t = Time.time * 1.8f;
+        float d = Mathf.Sin(t) * .01f + Mathf.Sin(t*1.3f) * .005f;
+        transform.localScale = originalSize +new Vector3(-d, d, 0);
+
+    }
+}
